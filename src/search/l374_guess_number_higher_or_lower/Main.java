04a77019ac5cba4763d6afb079a5f5b5eca785f3
tungnkg <2151173838@e.tlu.edu.vn>
2025-01-08 16:47:08 +0700

    l374_guess_number_higher_or_lower

---
+++ src/search/l374_guess_number_higher_or_lower/Main.java
@@ -0,0 +1,26 @@
+package search.l374_guess_number_higher_or_lower;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int guessNumber(int n) {
+        int l = 1;
+        int r = n;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (guess(mid) == 0) return mid;
+            if (guess(mid) == 1) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+
+    private static int guess(int mid) {
+        return 0;
+    }
+}
